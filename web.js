4c627ac0cd03d5364b4fb08129e916a620e4d3b1
vicner <vicner00@gmail.com>
2013-07-10 18:25:04 -0700

    Hello from index v.0.0.2

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 /*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
