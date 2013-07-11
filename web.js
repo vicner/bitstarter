8ed6f2e0edd4b210a3f885a26504fae391ed7bac
vicner <vicner00@gmail.com>
2013-07-10 18:20:18 -0700

    Hello from index v.0.0.1

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+*/
+
+app.get('/', function(request, response) {
+  var r = fs.readFileSync('./index.html'/*,'utf-8'*/);
+  response.end( r.toString() );
 });
 
 var port = process.env.PORT || 5000;
