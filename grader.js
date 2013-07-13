74f2e6ce7893cc42ccda9c0a7659513e6b109c05
vicner <vicner00@gmail.com>
2013-07-12 20:54:36 -0700

    updated to work with a url from command line

--- grader.js
+++ grader.js
@@ -1,31 +1,12 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "file:///index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +17,12 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(input, checksfile) {
+    $ = cheerio.load(input);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -54,6 +31,11 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+
+var outJson = function(inputJson) {
+return console.log(JSON.stringify(inputJson, null, 4));
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -64,11 +46,18 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to local index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_index>', 'Path to http://index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+if (program.url) {
+rest.get(program.url).on('complete', function(result) {
+     var checkJson = checkHtmlFile(result, program.checks);
+outJson(checkJson);
+});
+} else if (program.file) {
+     var checkJson = checkHtmlFile(fs.readFileSync(program.file), program.checks);
+outJsonConsole(checkJson);
+};
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
